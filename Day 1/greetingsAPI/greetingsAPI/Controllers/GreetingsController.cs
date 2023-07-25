8ad3e9e890da953026cdc6eeb624ef8f338fad1a
Nikhil <nikhilshah.net@hotmail.com>
2023-07-25 18:51:23 +0530

    Day 1

---
+++ Day 1/greetingsAPI/greetingsAPI/Controllers/GreetingsController.cs
@@ -0,0 +1,77 @@
+﻿using greetingsAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace greetingsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/greet")]
+        public IActionResult Greetings()
+        {
+            //methods will always return Http Status Code and Message/data
+
+            return Ok("Hello and welcome to webapi developers world");
+        }
+
+
+        [HttpGet]
+        [Route("/greet/{guestName}")]
+        public IActionResult Greetings(string guestName)
+        {
+            return Ok("Hello " + guestName);
+        }
+
+
+        [HttpGet]
+        [Route("/friends")]
+        public IActionResult Friendslist()
+        {
+            string[] friends = new string[5];
+            friends[0] = "Akshay";
+            friends[1] = "Priya";
+            friends[2] = "Riya";
+            friends[3] = "Jiya";
+            friends[4] = "Divya";
+            return Ok(friends);
+        }
+
+        [HttpGet]
+        [Route("/introduction")]
+        public IActionResult AboutMe()
+        {
+            var myInfo = new
+            {
+                firstName = "Nikhil",
+                lastName = "Shah",
+                city = "Mumbai",
+                edu = "MCA",
+                profession = "Consultant and trainer"                
+            };
+
+            return Ok(myInfo);
+
+        }
+
+        [HttpGet]
+        [Route("/product")]
+        public IActionResult GetProductInfo()
+        {
+            ProductDetails pObj = new ProductDetails(); //this is a crime, not a good practice, use DI instead
+            var data = pObj.Getproductinfo();
+            return Ok(data);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Day 1/greetingsAPI/greetingsAPI/Models/ProductDetails.cs
@@ -0,0 +1,32 @@
+﻿namespace greetingsAPI.Models
+{
+    public class ProductDetails
+    {
+        #region Properties
+        public int pId { get; set; }
+        public string pName { get; set; }
+         public string pDescription { get; set; }
+        public double pPrice { get; set; }
+
+        public bool pIsInStock { get; set; }
+        #endregion
+
+
+        #region Methods
+        public ProductDetails Getproductinfo()
+        {
+            ProductDetails pObj = new ProductDetails()
+            {
+                pId = 101,
+                pName = "Pepsi",
+                pDescription = "Cold Drink",
+                pIsInStock = true,
+                pPrice = 70
+            };
+            return pObj;
+        }
+        #endregion
+
+
+    }
+}
