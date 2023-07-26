054aed8cf86c720d36466da7d26061dce8579f92
Nikhil <nikhilshah.net@hotmail.com>
2023-07-26 18:29:33 +0530

    Day 2 - API with InMemory Collection

---
+++ Day 2/shoppingAPI_inMemoryCollection/shoppingAPI_inMemoryCollection/Controllers/ProductsController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using shoppingAPI_inMemoryCollection.Entities;
+namespace shoppingAPI_inMemoryCollection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        Products pObj = new Products(); //we should use DI instead
+
+        #region Get Products
+
+        [HttpGet]
+        [Route("/products")]
+        public IActionResult GetallProducts()
+        {
+            return Ok(pObj.GetAllProducts());
+        }
+
+
+        [HttpGet]
+        [Route("/products/id/{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            try
+            {
+                var prod = pObj.GetproductById(id);
+                return Ok(prod);
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("/products/category/{category}")]
+        public IActionResult GetProductByCategory(string category)
+        {
+            try
+            {
+                var products = pObj.GetProductByCategory(category);
+                return Ok(products);
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("/products/total")]
+        public IActionResult GettotalProducts()
+        {
+            var total = pObj.GetTotalProducts();
+            return Ok(total);
+        }
+
+        #endregion
+
+        #region Add, Update and Delete
+
+        [HttpPost]
+        [Route("/products/add")]
+        public IActionResult AddProduct(Products newProduct)
+        {
+            var result = pObj.AddProduct(newProduct);
+            return Created("", result);
+        }
+        [HttpPut]
+        [Route("/products/edit")]
+        public IActionResult UpdateProduct(Products changes)
+        {
+            try
+            {
+                var result = pObj.UpdateProduct(changes);
+                return Accepted(result);
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+        [HttpDelete]
+        [Route("/products/delete/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                var result = pObj.DeleteProduct(id);
+                return Accepted(result);
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+                
+            }
+        }
+        #endregion
+    }
+
+}
+
+
+
+
+
+
---
+++ Day 2/shoppingAPI_inMemoryCollection/shoppingAPI_inMemoryCollection/Entities/Products.cs
@@ -0,0 +1,111 @@
+﻿using System.Globalization;
+
+namespace shoppingAPI_inMemoryCollection.Entities
+{
+    public class Products
+    {
+
+        #region Properties
+        public int pId { get; set; }
+
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public string pDescription { get; set; }
+        public double pPrice { get; set; }
+
+        public bool pIsInStock { get; set; }
+        #endregion
+
+        #region Data
+        static List<Products> pList = new List<Products>()
+        {
+            new Products(){ pId=101, pName="Pepsi", pCategory="Cold-Drink", pDescription="Chilled Black water to kill the thirst", pIsInStock=true, pPrice=80},
+            new Products(){ pId=102, pName="IPhone", pCategory="Electronics", pDescription="Make Smart calls", pIsInStock=false, pPrice=80},
+            new Products(){ pId=103, pName="Dell Lattitude", pCategory="Electronics", pDescription="Working Professionals", pIsInStock=true, pPrice=80},
+            new Products(){ pId=104, pName="Fossil Q", pCategory="Electronics", pDescription="Check the time", pIsInStock=true, pPrice=80},
+            new Products(){ pId=105, pName="Nike Air", pCategory="Sports", pDescription="Win the race - do it right", pIsInStock=false, pPrice=80},
+            new Products(){ pId=106, pName="Adidas", pCategory="Sports", pDescription="Beat the race", pIsInStock=true, pPrice=80},
+        };
+        #endregion
+
+        #region Methods
+        public List<Products> GetAllProducts()
+        {
+            return pList;
+        }
+
+        public Products GetproductById(int id)
+        {
+            var prod = pList.Find(pr => pr.pId == id);
+            if (prod != null)
+            {
+                return prod;
+            }
+            throw new Exception("Product Not Found");
+        }
+
+        public List<Products> GetProductByCategory(string category)
+        {
+            var products = pList.FindAll(pr => pr.pCategory == category);
+            if (products.Count > 1)
+            {
+                return products;
+            }
+            throw new Exception("No Product under this category");
+        }
+
+        public int GetTotalProducts()
+        {
+            return pList.Count;
+        }
+
+        public string AddProduct(Products newProductDetails)
+        {
+            //do some data validation, calculation, formatting if needed
+            pList.Add(newProductDetails);
+            return "Product Added Successfully";
+        }
+
+        public string DeleteProduct(int pId)
+        {
+            var prod = pList.Find(pr => pr.pId == pId);
+            if (prod != null)
+            {
+                pList.Remove(prod);
+                return "Product Deleted Successfully";
+            }
+            throw new Exception("Product Not Found");
+        }
+
+        public string UpdateProduct(Products changes)
+        {
+            var prod = pList.Find(pr => pr.pId == changes.pId);
+            if (prod != null)
+            {
+                prod.pName = changes.pName;
+                prod.pPrice = changes.pPrice;
+                prod.pCategory = changes.pCategory;
+                prod.pIsInStock = changes.pIsInStock;
+                prod.pDescription = changes.pDescription;
+
+                return "Product Updated Successfully";
+            }
+            throw new Exception("Product Not Found");
+
+        }
+
+        #endregion
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
