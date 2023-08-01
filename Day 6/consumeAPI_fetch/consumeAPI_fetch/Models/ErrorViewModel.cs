2225a9e33280cdab429e284bb4ff129de987286f
Nikhil <nikhilshah.net@hotmail.com>
2023-08-01 18:48:35 +0530

    Day 5 - Fetch API calls

---
+++ Day 6/consumeAPI_fetch/consumeAPI_fetch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace consumeAPI_fetch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
